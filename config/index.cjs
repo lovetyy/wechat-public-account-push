37e8b88d0fb0b6a06ad9ea2a9d61f9a523877efe
lovetyy <118000332+lovetyy@users.noreply.github.com>
2022-11-13 21:53:38 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -25,7 +25,7 @@ const USER_CONFIG = {
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'U1y3qra5lN5mg2BhMMQohrPzOQms83RDkhsslsC6TuE',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
+      horoscopeDate: '10-10',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
