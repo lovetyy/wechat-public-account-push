756f78a557a88b13ce12db6ab9e864f3520ab215
lovetyy <118000332+lovetyy@users.noreply.github.com>
2022-11-27 09:07:52 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝糖',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'ow6Rg6R962R37DeiGlzKUOhjJHDo',
+      id: 'ow6Rg6QbD3Gw3IP9ax7FY0ASI_VU',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '9KKiJJemKUs2YK15_oi8iCJih3RJMIE1ExwM2adWmIE',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
