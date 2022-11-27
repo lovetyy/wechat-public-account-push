91e4c754c39c7e22929af94189d6cac6362dc533
lovetyy <118000332+lovetyy@users.noreply.github.com>
2022-11-27 08:57:50 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'ow6Rg6R962R37DeiGlzKUOhjJHDo',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'ams0zxS0a_QgobnhiTmc87RgFLvvMhzHUNJ9iTiVlTo',
+      useTemplateId: 'lnJWX2kmkxg6Wgu_yc2tDhDe6NoGKtedJELLmhxwRjY',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '10-10',
       festivals: [
